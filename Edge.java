17ac0b97afd7414663dcb2cf991471aed0d09257
TKCoder13 <TKCoding13@gmail.com>
2023-05-04 17:47:18 -0700

    starting Edge

--- Edge.java
+++ Edge.java
@@ -1,3 +1,36 @@
 public class Edge {
     
+    private String start;
+    private String end;
+    private int weight;
+
+    public Edge(String inStart, String inEnd, int inWeight) {
+        start = inStart;
+        end = inEnd;
+        weight = inWeight;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setStart(String input) {
+        start = input;
+    }
+
+    public void setEnd(String input) {
+        end = input;
+    }
+
+    public void setWeight(int input) {
+        weight = input;
+    }
 }
